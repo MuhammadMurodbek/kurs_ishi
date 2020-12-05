f978b3fa08eccce1b944708148b9c13b5fbf6403
Murodbek <bekbruno99@gmail.com>
2020-12-05 23:08:49 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React from 'react'
 import './App.css';
+import Product from './components/Product';
+import data from "./data.json"
 
-function App() {
-  return (
-    <div className="grid-container">
-       <header>
-         <a href="/">React shopping cart</a>
-       </header>
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      products:data.products,
+      size:"",
+      sort:"",
+    }
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React shopping cart</a>
+        </header>
 
-       <main>
-         Product list
-       </main>
-
-       <footer>
-         All right is reserved
-       </footer>
-    </div>
-  );
+        <main>
+            <div className="content">
+              <div className="main">
+                <Product products={this.state.products}/>
+              </div>
+              <div className="sidebar">Cart Items</div>
+            </div>
+        </main>
+          
+        <footer>
+          All right is reserved
+        </footer>
+      </div>
+    );
+  }  
 }
 
 export default App;
+///https://www.youtube.com/watch?v=731Ur2HGRBY
